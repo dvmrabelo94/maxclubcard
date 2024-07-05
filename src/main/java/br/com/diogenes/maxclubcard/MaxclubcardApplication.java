2bc06426f8001b0c8ec23b1ac258122b1363278f
Diogenes Victor Rabelo <diogenesrabelo94@gmail.com>
2024-07-05 16:11:03 -0300

    Iniciando o teste de tecnologia

---
+++ src/main/java/br/com/diogenes/maxclubcard/MaxclubcardApplication.java
@@ -0,0 +1,13 @@
+package br.com.diogenes.maxclubcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxclubcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxclubcardApplication.class, args);
+	}
+
+}
